26797a56be6f3a5486c0b246cbcd8588f44cb4be
isys35 <isys35@mail.ru>
2019-12-05 12:03:19 +0300

    парсер категорий до товара

--- main.py
+++ main.py
@@ -10,20 +10,43 @@ class MainApp(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.comboBox.addItem('https://happygifts.ru/')
-        self.pushButton.clicked.connect(self.update_category)
+        self.pushButton.clicked.connect(self.update_categorie)
+        self.listWidget.itemClicked.connect(self.update_subcategorie)
+        self.listWidget_3.itemClicked.connect(self.update_goods)
+        self.categories = []
+        self.goods = []
 
-    def update_category(self):
+    def update_categorie(self):
         if self.comboBox.currentText() == 'https://happygifts.ru/':
-            categories = HappyGifts().parser_category()
-            print(categories)
-            if self.listWidget.count()>1:
-                for i in range(0,self.listWidget.count()):
-                    self.listWidget.removeItemWidget(self.listWidget.item(i))
-            for categorie in categories:
+            self.categories = HappyGifts().parser_category()
+            print(self.categories)
+            if self.listWidget.count() > 0:
+                self.listWidget.clear()
+            for categorie in self.categories:
                 item = QtWidgets.QListWidgetItem()
-                item.setText(categorie)
+                item.setText(categorie['title'])
                 self.listWidget.addItem(item)
 
+    def update_subcategorie(self):
+        if self.listWidget_3.count() > 0:
+            self.listWidget_3.clear()
+        for item in self.listWidget.selectedItems():
+            it = QtWidgets.QListWidgetItem()
+            it.setText(item.text())
+            self.listWidget_3.addItem(it)
+
+    def update_goods(self):
+        if self.listWidget_2.count() > 0:
+            self.listWidget_2.clear()
+            self.goods = []
+        for item in self.listWidget_3.selectedItems():
+            for categorie in self.categories:
+                if categorie['title'] == item.text():
+                    if self.comboBox.currentText() == 'https://happygifts.ru/':
+                        goods = HappyGifts().parser_goods(categorie['href'])
+                        for good in goods:
+                            self.goods.append(good)
+                            self.listWidget_2.addItem(good['title'])
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = MainApp()
--- parser_happygifts.py
+++ parser_happygifts.py
@@ -12,7 +12,25 @@ class HappyGifts:
         categories = []
         for el in html.select('.catalog-block'):
             title = el.select('.title')
-            categories.append(title[0].text)
-
-
+            categories.append({'title': title[0].text, 'href': title[0]['href']})
         return categories
+
+    def parser_goods(self, href):
+        first_page = self.main_page + href[1:]
+        r = requests.get(first_page)
+        html = BS(r.content, 'html.parser')
+        goods = []
+        navigation = html.select('.catalog-pagination.modern-page-navigation')
+        max_page = navigation[0].select('a')
+        max_page = int(max_page[-2].text)
+        for i in range(1, max_page+1):
+            print('cтраница %i' %i)
+            r = requests.get(first_page+'?PAGEN_1=' + str(i))
+            html = BS(r.content, 'html.parser')
+            for el in html.select('.catalog-item-container'):
+                text_container = el.select('.text-container')
+                if text_container:
+                    title = text_container[0].select('.product-title')
+                    goods.append({'title': title[0].text, 'href': title[0]['href']})
+        print(goods)
+        return goods
